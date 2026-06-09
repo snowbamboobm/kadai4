08543aa7e6a600dc74ab6102aedcfb76d02368a2
yukitakeM <a26139my@aiit.ac.jp>
2026-06-09 21:26:09 +0900

    test1-2の値修正

--- test-gcd.sh
+++ test-gcd.sh
@@ -19,7 +19,7 @@ ERROR_EXIT () {
 diff $tmp-ans $tmp-args || ERROR_EXIT "error in test1-1-2"
 
 #test1-2：引数の数が多い
-./gcd.sh 1 2 3> $tmp-ans && ERROR_EXIT "error in test1-2-1"
+./gcd.sh 13 4 25> $tmp-ans && ERROR_EXIT "error in test1-2-1"
 diff $tmp-ans $tmp-args || ERROR_EXIT "error in test1-2-2"
 
 #test2：引数が小数
@@ -41,31 +41,31 @@ diff $tmp-ans $tmp-over || ERROR_EXIT "error in test5-2"
 #test6-1：引数が正常
 ./gcd.sh 2 4> $tmp-result
 echo "2" > $tmp-ans
-diff $tmp-reselt $tmp-ans && echo "OK" || ERROR_EXIT "error in test6-1"
+diff $tmp-result $tmp-ans && echo "OK" || ERROR_EXIT "error in test6-1"
 rm $tmp-result $tmp-ans
 
 #test6-2：引数が正常
 ./gcd.sh 12 18> $tmp-result
 echo "6" > $tmp-ans
-diff $tmp-reselt $tmp-ans && echo "OK" || ERROR_EXIT "error in test6-2"
+diff $tmp-result $tmp-ans && echo "OK" || ERROR_EXIT "error in test6-2"
 rm $tmp-result $tmp-ans
 
 #test6-3：引数が正常
 ./gcd.sh 7 11> $tmp-result
 echo "1" > $tmp-ans
-diff $tmp-reselt $tmp-ans && echo "OK" || ERROR_EXIT "error in test6-3"
+diff $tmp-result $tmp-ans && echo "OK" || ERROR_EXIT "error in test6-3"
 rm $tmp-result $tmp-ans
 
 #test6-4：引数が正常
 ./gcd.sh 100 90> $tmp-result
 echo "10" > $tmp-ans
-diff $tmp-reselt $tmp-ans && echo "OK" || ERROR_EXIT "error in test6-4"
+diff $tmp-result $tmp-ans && echo "OK" || ERROR_EXIT "error in test6-4"
 rm $tmp-result $tmp-ans
 
 #test6-5：引数が正常
 ./gcd.sh 50 75> $tmp-result
 echo "25" > $tmp-ans
-diff $tmp-reselt $tmp-ans && echo "OK" || ERROR_EXIT "error in test6-5"
+diff $tmp-result $tmp-ans && echo "OK" || ERROR_EXIT "error in test6-5"
 rm $tmp-result $tmp-ans
 
 
